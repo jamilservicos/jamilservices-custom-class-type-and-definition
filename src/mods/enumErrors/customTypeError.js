15f5c8ce07e601cc04d8e0e951ab3448edc17299
JamilServices <jamilservicos@gmail.com>
2023-12-19 20:43:23 -0300

    Add customTypeError to enumErrors object

--- src/mods/enumErrors/customTypeError.js
+++ src/mods/enumErrors/customTypeError.js
@@ -1,18 +1,24 @@
 "use strict";
 
 /**
- * @name customTypeError
+ * Custom TypeError function.
+ *
+ * This function is designed to log a TypeError message specific to a custom type
+ * and return a boolean indicating the error occurrence.
+ *
  * @function
- * @param {string} customType
- * @returns {boolean}
+ * @name  customTypeError
+ * @param {string} customType - The name of the custom type that caused the error.
+ * @returns {boolean} Returns false to indicate that a type error has occurred.
  */
 const customTypeError = (customType) => {
     console.log("typeOf error: TypeError on " + customType);
     return false;
 };
+
 /**
  * @private
- * @inheritDoc
+ * @description Exports the customTypeError function.
  */
 exports = module.exports = {
     customTypeError
--- src/mods/enumErrors/index.js
+++ src/mods/enumErrors/index.js
@@ -1,13 +1,24 @@
 "use strict";
 
 /**
+ * Enumerated errors object.
+ *
+ * This object holds a collection of different error types
+ * for the application. Each key in this object represents a specific error type
+ * and is associated with a corresponding error handling module.
+ *
  * @name enumErrors
+ * @property {Function} CUSTOMTYPEERROR - The function imported from "./customTypeError" module,
+ *                                        used to handle custom type errors.
  */
 const enumErrors = {
     "CUSTOMTYPEERROR": require("./customTypeError")
 };
+
 /**
  * @private
+ * @description Exports the enumErrors object.
+ *              This object is meant for internal use within the module or package.
  */
 exports = module.exports = {
     ...{enumErrors}
