40dffb0211087cb0be3581977fc54ee3cc2c3092
JamilServices <jamilservicos@gmail.com>
2023-12-19 06:43:03 -0300

    Add new file test/setup.js with setup and teardown functions for tests

---
+++ test/setup.js
@@ -0,0 +1,11 @@
+"use strict";
+
+before(() => {
+    console.log("======== START TESTS ========");
+});
+describe('TESTS TO: CustomTypeInterface, CustomTypeInterfaceDefinition, registerDefinition', () => {
+    it('=======================',  () => true);
+});
+after(() => {
+    console.log("======== END TESTS ========");
+});
