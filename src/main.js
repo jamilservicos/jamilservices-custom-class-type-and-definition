a0beb118351f7f3f6d26a63698b2661e1b5f5bd1
JamilServices <jamilservicos@gmail.com>
2023-12-19 20:42:14 -0300

    Add main module file serving as entry point with re-exported contents from './mods'.

--- src/main.js
+++ src/main.js
@@ -1,6 +1,12 @@
 "use strict";
 
 /**
+ * Main file of the module.
+ *
+ * This file serves as the entry point for the module, re-exporting all the contents from the './mods' file.
+ * It is set up to be private and should be used as part of the module's internal logic.
+ *
+ * @module MainModule
  * @private
  */
 exports = module.exports = require("./mods");
