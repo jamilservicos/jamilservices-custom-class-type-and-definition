b44c7203e7ea7b5ef42a1d0bc9c028ff549aeed7
JamilServices <jamilservicos@gmail.com>
2023-12-20 06:09:48 -0300

    Change module name from 'MainModule' to 'CustomClassTypeAndDefinition'.

--- src/main.js
+++ src/main.js
@@ -6,7 +6,7 @@
  * This file serves as the entry point for the module, re-exporting all the contents from the './mods' file.
  * It is set up to be private and should be used as part of the module's internal logic.
  *
- * @module MainModule
+ * @module CustomClassTypeAndDefinition
  * @private
  */
 exports = module.exports = require("./mods");
